93f2daa638366d8025a4b48968ce65e73b8d2e6d
Gagan Hegde <48808456+gaganhegde@users.noreply.github.com>
2022-05-03 01:11:28 -0400

    Update setup.sh

--- pipeline/scripts/setup.sh
+++ pipeline/scripts/setup.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-echo "test-1"
 
 #FETCH SECRET FROM SECRET MANAGER
 
@@ -14,22 +13,26 @@ if ibmcloud login --apikey $IBMCLOUD_API_KEY -r "$IBM_CLOUD_REGION"  ;then
     echo "Logged into IBM clouds sucessfully"
 else
     echo "could not log into IBM cloud"
+    exit
 fi
 
 if ibmcloud plugin install secrets-manager  ;then
     echo "Installed the secrets manager sucessfully"
 else
     echo "failed to install the secrets manager"
+    exit
 fi
 
 ibmcloud secrets-manager secret --secret-type "arbitrary" --id "ebf45e88-6650-4c91-e795-d11250577278" --service-url https://c30c55a3-7252-47aa-aeb9-8d21da464f45.us-south.secrets-manager.appdomain.cloud --output json > ssh_auth.txt
 
-cat ssh_auth.txt
-if jq --help  ;then
-    echo "jq is installed on the system"
+if jq '.resources[] | select(.name=="test-auth") | .secret_data.payload' ssh_auth.txt > ssh_auth_secret.txt  ;then
+    echo "The ssh auth token has been sucessfully preserved"
 else
-    echo "jq is not present on the system"
+    echo "Secret 'test-auth' not present"
+    exit
 fi
+
+cat ssh_auth_secret.txt
 
 # $WORKSPACE is shared between steps
 python3 -m venv $WORKSPACE/virtual/environment
