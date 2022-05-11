e6c3a5479a857eb842b8fb0741272602f93450d6
Gagan Hegde <48808456+gaganhegde@users.noreply.github.com>
2022-05-11 17:12:37 -0400

    Update release.sh

--- pipeline/scripts/release.sh
+++ pipeline/scripts/release.sh
@@ -12,6 +12,8 @@ echo "printing sample.log"
 cat sample.log
 
 echo $(get_env cos-api-key "") $(get_env cos-bucket-name "") $(get_env cos-endpoint "") "running pipeline"
+echo "testing evidence info"
+ehco $(get_env evidence-info "")
 cocoa --version
 echo "evidence upload-1"
 cocoa evidence upload --backend=cos --evidence-name=zopeneditor-logs --COS_API_KEY=$(get_env cos-api-key "") --COS_BUCKET_NAME=$(get_env cos-bucket-name "") --COS_ENDPOINT=$(get_env cos-endpoint "")
