e816ac8757236c1c57fcb343d565769699b7639a
Gagan Hegde <48808456+gaganhegde@users.noreply.github.com>
2022-05-11 02:51:36 -0400

    Update release.sh

--- pipeline/scripts/release.sh
+++ pipeline/scripts/release.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
+
+#Store an evidence within the bucket storage
+
+COS-API-KEY="$(get_env cos-api-key "")"
+COS-BUCKET-NAME="$(get_env cos-bucket-name "")"
+COS-ENDPOINT="$(get_env cos-endpoint "")"
+
 echo "testing the cocoa cli"
-cocoa --help
+cat >> sample.log << 'END'
+line-1
+line-2
+END
+
+echo $COS-API-KEY $COS-BUCKET-NAME $COS-ENDPOINT
+cocoa evidence upload --COS_API_KEY=$COS-API-KEY --COS_BUCKET_NAME=$COS-BUCKET-NAME --COS_ENDPOINT=$COS-ENDPOINT --artifact=./sample.log
