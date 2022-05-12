17944bdbe5f0b4a7413d496c9dc5f3926f8507ce
Gagan Hegde <48808456+gaganhegde@users.noreply.github.com>
2022-05-12 10:32:15 -0400

    Update release.sh

--- pipeline/scripts/release.sh
+++ pipeline/scripts/release.sh
@@ -11,8 +11,18 @@ END
 echo "printing sample.log"
 cat sample.log
 echo "just fetching some env variables"
+echo "pipeline ID"
+echo "${PIPELINE_ID}"
+echo "pipeline run url"
+echo "${PIPELINE_RUN_URL}"
+echo "pipeline run ID"
 echo "${PIPELINE_RUN_ID}"
+echo "toolchain crn"
 echo "${TOOLCHAIN_CRN}"
+echo "evidence type version"
+echo "${EVIDENCE_TYPE_VERSION}"
+echo "evidence type"
+echo "${EVIDENCE_TYPE}"
 echo $(get_env cos-api-key "") $(get_env cos-bucket-name "") $(get_env cos-endpoint "") "running pipeline"
 # echo "testing evidence info"
 # echo $(get_env evidence-info "")
